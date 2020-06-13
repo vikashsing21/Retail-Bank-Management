90c52d341d1ea94cf7679449c2b6ffa1063ff90a
ShADow <shlokgadre@gmail.com>
2020-06-13 17:04:24 +0530

    logout, create, update

--- appmongo.py
+++ appmongo.py
@@ -1,18 +1,20 @@
 from flask import Flask, jsonify, render_template, request, session, redirect, url_for
 from flask_pymongo import PyMongo
 
+# app initailization
 app = Flask(__name__)
 app.secret_key = 'secretkey'
-app.config['MONGO_URI'] = "mongodb://127.0.0.1:27017/dbtest"
 
+# database path
+app.config['MONGO_URI'] = "mongodb://127.0.0.1:27017/dbtest"
 mongo = PyMongo(app)
 
-
+# index page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+# errorhandler
 @app.errorhandler(404)
 def not_found(error=None):
     message = {
@@ -26,6 +28,12 @@ def not_found(error=None):
     return resp
 
 
+@app.route('/logout')
+def logoutUser():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
 @app.route('/login.html')
 def indexLogin():
     return render_template('login.html')
@@ -35,7 +43,7 @@ def indexLogin():
 def indexSignup():
     return render_template('signup.html')
 
-
+# checks if the user is authenticate user or not
 @app.route('/login', methods=['POST'])
 def findEmp():
     usr = request.form['username']
@@ -55,7 +63,7 @@ def findEmp():
     else:
         return not_found()
 
-
+# inserts new user in the database
 @app.route('/signup', methods=['POST'])
 def createEmp():
     usr = request.form['username']
@@ -78,6 +86,53 @@ def goToOptions():
             return render_template("options.html")
         else:
             return not_found()
+    else:
+        return not_found()
+
+
+@app.route('/createCust.html')
+def createCust():
+    if 'username' in session:
+        if session['username'] == 'auth':
+            return render_template("createCust.html")
+        else:
+            return not_found()
+    else:
+        return not_found()
+
+
+@app.route('/updateCust.html')
+def updateCust():
+    if 'username' in session:
+        if session['username'] == 'auth':
+            return render_template("updateInit.html")
+        else:
+            return not_found()
+    else:
+        return not_found()
+
+
+@app.route('/delCust.html')
+def delCust():
+    if 'username' in session:
+        if session['username'] == 'auth':
+            return render_template("delCust.html")
+        else:
+            return not_found()
+    else:
+        return not_found()
+
+
+@app.route('/crud/<id>', methods=['GET', 'POST', 'DELETE'])
+def crudCust(id):
+    if id == 'create':
+        return 'hello'
+    elif id == 'update':
+        return request.form['ssnid']
+    elif id == 'delete':
+        return 'delete'
+    elif id == "fetchCust":
+        return render_template('updateCust.html', a='1234', b='shlok', c='mumbai', d='18')
     else:
         return not_found()
 
